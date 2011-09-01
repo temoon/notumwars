27a29fd7f4dde2843282da3fe139e3d93af65c35
Tema Novikov <temoon@tema.(none)>
2011-09-01 18:57:57 +0400

    Just complete...

--- notumwars.py
+++ notumwars.py
@@ -3,27 +3,83 @@
 
 
 import sys
+import yaml
+import threading
+import oauth
+import oauthtwitter
 
-from aochat import Chat, ChatError, AOSP_CHARACTER_NAME, AOSP_CHANNEL_MESSAGE
-from aochat.dimensions import DIMENSIONS
-from aochat.data import load_texts
-
-
-CHARACTERS = {}
-CHANNEL_ALL_TOWERS = 42949672960L
+from aochat import Chat, ChatError, AOSP_CHANNEL_MESSAGE
 
 
-def callback(chat, packet):
+class Worker(threading.Thread):
     """
-    AOChat callback.
+    Thread worker.
     """
     
-    # Cache character name
-    if packet.type == AOSP_CHARACTER_NAME.type:
-        CHARACTERS[packet.character_id] = packet.character_name
+    sides = [
+        "neutral",
+        "clan",
+        "omni",
+    ]
     
-    if packet.type == AOSP_CHANNEL_MESSAGE.type and packet.category and packet.instance:
-        print packet.mask % tuple(packet.args)
+    def __init__(self, username, password, host, port, character, tag, twitter):
+        threading.Thread.__init__(self)
+        
+        self.username  = username
+        self.password  = password
+        self.host      = host
+        self.port      = port
+        self.character = character
+        self.tag       = tag
+        self.twitter   = twitter
+    
+    def run(self):
+        while True:
+            try:
+                chat = Chat(self.username, self.password, self.host, self.port)
+                chat.login(chat.characters[self.character].id)
+                chat.start(self.callback)
+            except ChatError:
+                continue
+            except SystemExit:
+                break
+    
+    def callback(self, chat, packet):
+        # Check packet
+        if not (
+            packet.type       == AOSP_CHANNEL_MESSAGE.type and  # Channel message
+            packet.channel_id == 42949672960 and                # "All Towers" channel
+            packet.category   == 506 and                        # Message category
+            packet.instance   == 12753364                       # Message instance
+        ):
+            return
+        
+        # Sides:
+        # 2005 0 neutral
+        # 2005 1 clan
+        # 2005 2 omni
+        
+        # The %s organization %s just entered a state of war! %s attacked the %s organization %s's tower in %s at location (%d,%d).
+        # 0 - Attacker's org. side
+        # 1 - Attacker's org. name
+        # 2 - Attacker's name
+        # 3 - Defender's org. side
+        # 4 - Defender's org. name
+        # 5 - Area name
+        # 6 - X
+        # 7 - Y
+        
+        # Org A (omni) ⚔ Org B (clan), Location (100,500), #RKx
+        self.twitter.PostUpdate("%s (%s) \xe2\x9a\x94 %s (%s), %s (%d,%d), #%s" % (
+            sides[packet.args[0][1]],
+            packet.args[1],
+            sides[packet.args[3][1]],
+            packet.args[4],
+            packet.args[5],
+            packet.args[6],
+            packet.args[7],
+            self.tag
+        ))
 
 
 def main(argv = []):
@@ -31,21 +87,32 @@ def main(argv = []):
     Launcher.
     """
     
-    username = ""
-    password = ""
+    # Read settings
+    config = yaml.load(file("notumwars.conf", "rb"))
     
-    dimension = DIMENSIONS[2]
+    # Init Twitter API
+    access_token = oauth.oauth.OAuthToken(config["twitter"]["access_token_key"], config["twitter"]["access_token_secret"])
+    twitter      = oauthtwitter.OAuthApi(config["twitter"]["consumer_key"], config["twitter"]["consumer_secret"], access_token)
     
-    load_texts("/var/lib/aochat/texts.dat")
+    # Start workers
+    workers = []
     
-    while True:
-        try:
-            chat = Chat(username, password, dimension.host, dimension.port)
-            character = chat.characters[1]
-            chat.login(character.id)
-            chat.start(callback)
-        except ChatError, error:
-            print error
+    for dimension in config["ao"]["dimensions"]:
+        worker = Worker(
+            username  = config["ao"]["username"],
+            password  = config["ao"]["password"],
+            host      = dimension["host"],
+            port      = dimension["port"],
+            character = dimension["character"],
+            tag       = dimension["tag"],
+            twitter   = twitter,
+        )
+        
+        worker.start()
+        workers.append(worker)
+    
+    for worker in workers:
+        worker.join()
     
     return 0
 
